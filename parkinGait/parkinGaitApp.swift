657c1a35c365090ba1ba64e398c103e178196a5b
Changmin Shin <cs521@duke.edu>
2024-06-22 15:48:38 +0900

    Initial Commit

---
+++ parkinGait/parkinGaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  parkinGaitApp.swift
+//  parkinGait
+//
+//  Created by 신창민 on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct parkinGaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
