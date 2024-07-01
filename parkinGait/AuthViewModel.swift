0334e1213f705d2ce70b9900885e838b62911978
Changmin Shin <cs521@duke.edu>
2024-07-01 17:03:00 +0900

    create AuthViewModel, implement in views

---
+++ parkinGait/AuthViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewModel.swift
+//  parkinGait
+//
+//  Created by 신창민 on 7/1/24.
+//
+
+import SwiftUI
+
+struct AuthViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthViewModel()
+}
